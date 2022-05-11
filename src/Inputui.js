accb5d801de3223ca9b3cade92cbe1d23dafc0da
Divye Gupta <divyegupta00@gmail.com>
2022-05-11 22:13:19 +0530

    add: background and style

--- src/Inputui.js
+++ src/Inputui.js
@@ -2,18 +2,18 @@ import React from "react";
 import "./css/Inputui.css";
 import Recorder from "./Recorder";
 const Inputui = () => {
-    const upload= ()=>{
-        console.log('hello');
-    }
+  const upload = () => {
+    console.log("hello");
+  };
   return (
     <div className="inputbox">
-        <h1>Let's hear what you say</h1>
+      <h1>Let's hear what you say</h1>
       <button onClick={upload}>Upload your Audio</button>
       <hr />
-    <h1>OR</h1>
+      <h1>OR</h1>
       <hr />
       <h1>Record an audio</h1>
-      <Recorder/>
+      <Recorder />
     </div>
   );
 };
---
+++ src/Postreq.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Postreq = () => {
+    return(
+        <div>
+            
+        </div>
+    )
+}
+export default Postreq;
